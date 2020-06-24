318c0bdf73589d4280fdc3ed59b4e342e80a1524
Karolina <61783738+Karolina-beep@users.noreply.github.com>
2020-06-24 17:08:10 +0200

    Dodałam kod narysowania całej funkcji.

--- 3.py
+++ 3.py
@@ -28,7 +28,41 @@ radius = 120
 offset = 300
 wave_list = []
 ITERATIONS = 5
-#stworzyć kod na rysowanie całej funkcji 
 
+run=True
+while run:
+    clock.tick(fps)
+    screen.fill(white)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run=False
+    x = pos_x
+    y = pos_y
+    for i in range(ITERATIONS):
+        old_x = x
+        old_y = y
+
+        N = i * 2 + 1
+        radius = 150 * (4/ (N * math.pi))
+        x += int( radius * math.cos(N * time))
+        y +=  int( radius * math.sin(N * time))
+
+
+        pygame.draw.circle(screen, gray, (old_x, old_y), int(radius) ,2)
+
+        pygame.draw.line(screen, black, (old_x, old_y), (x,y) , 3)
+        pygame.draw.circle(screen, green, (x,y), 5)
+
+    wave_list.insert(0, y)
+    if len(wave_list) > 1000:
+        wave_list.pop()
+
+    pygame.draw.line(screen, gray, (x,y), (pos_x+offset, wave_list[0]), 3)
+
+    for index in range(len(wave_list)):
+        pygame.draw.circle(screen, gray2, (index + pos_x + offset, wave_list[index]), 3)
+    time += 0.01
+
+    pygame.display.update()
 
 pygame.quit()
